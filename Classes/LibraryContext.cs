c316cb0f83345ad4fec58ea2f3e59f204fac8cfc
AnastasiaBahinska <anastasia.bahinska@pragmaticdlt.com>
2024-04-21 17:51:24 +0300

    Add project files.

---
+++ Classes/LibraryContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Library
+{
+    internal class LibraryContext : DbContext
+    {
+        public LibraryContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+    public DbSet<Record> Records { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;Database=Library_Project;AttachDbFilename=D:\Downloads\Library.mdf;Integrated Security=True;Connect Timeout=30");
+            }
+        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+            modelBuilder.Entity<Book>()
+                .HasOne(b => b.Genre)
+                .WithMany(g => g.Books);
+
+        modelBuilder.Entity<Record>()
+            .HasOne(r => r.User)
+            .WithMany(u => u.Records)
+            .HasForeignKey(r => r.UserId);
+
+        modelBuilder.Entity<Record>()
+            .HasOne(r => r.Book)
+            .WithMany(b => b.Records)
+            .HasForeignKey(r => r.BookId);            
+    }
+
+}
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Library;
+
+using (LibraryContext context = new LibraryContext())
+{
+    // Add a user
+    var user = new User
+    {
+        Username = "john_doe",
+        PhoneNumber = "123456789",
+        IsAdmin = false
+    };
+    context.Users.Add(user);
+
+    // Add a genre
+    var genre = new Genre
+    {
+        Name = "Science Fiction"
+    };
+    context.Genres.Add(genre);
+
+    // Add a book
+    var book = new Book
+    {
+        Name = "The Hitchhiker's Guide to the Galaxy",
+        Author = "Douglas Adams",
+        IsAvailable = true,
+    };
+    context.Books.Add(book);
+
+    // Add a record
+    var record = new Record
+    {
+        UserId = user.Id, // Set the user ID
+        BookId = book.Id, // Set the book ID
+        Status = RecordStatus.Open,
+        CreatedAt = DateTime.Now
+    };
+    context.Records.Add(record);
+
+    // Save changes to the database
+    context.SaveChanges();
+}
+  
